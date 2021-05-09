9995bdb2e2a70fce1254122320c1314e9b7da760
rafaelst97 <42190573+rafaelst97@users.noreply.github.com>
2021-05-09 15:45:34 -0300

    Problemas na criação da tabela hash

--- trabalho_hash/leitura_arquivo.h
+++ trabalho_hash/leitura_arquivo.h
@@ -6,17 +6,22 @@
 
 using namespace std;
 
-void primeira_hash(int& palavra_ascii) {
+void primeira_hash(int& palavra_ascii, string &palavra) { //Primeiro metodo hash, recebe como parametro a palavra lida do texto menor
 
-	int texto_ascii[25]; // Vetor para separar cada palavra
+	string texto[25]; // Tabela
+	for (int i = 0; i < 25; i++) {
+		texto[i] = "";
+	}
 	int navegacao_texto = 0; // Variavel para auxiliar na navegacao do vetor e imprimir o mesmo
 
-	texto_ascii[navegacao_texto] = palavra_ascii; //Se a palavra termnou, ela eh colocada no vetor do texto
-	palavra_ascii = 0; // Zerando o valor da palavra
-	navegacao_texto++; // Parte para a proxima posicao do vetor de texto
+	navegacao_texto = palavra_ascii % 100;
+	
+	while (texto[navegacao_texto] != "")
+		texto[navegacao_texto] = palavra; //Se a palavra termnou, ela eh colocada no vetor do texto
+		palavra_ascii = 0; // Zerando o valor da palavra
 
 	for (int i = 0; i < navegacao_texto; i++) { // Impressao do vetor de texto
-		cout << texto_ascii[i] << endl;
+		cout << texto[i] << endl;
 	}
 
 }
@@ -25,6 +30,8 @@ void leitura_do_arquivo_menor() { // Funcao para ler arquivo menor
 
 	ifstream arquivo_menor; //Arquivo com no maximo 25 palavras
 	string linha; //Variavel para auxiliar na navegacao do arquivo
+	string palavra; //Variavel para salvar palavras individualmente
+	palavra.clear();
 	int palavra_ascii = 0; // Variavel para somar valores ASCII
 
 	
@@ -36,11 +43,12 @@ void leitura_do_arquivo_menor() { // Funcao para ler arquivo menor
 
 			for (int i = 0; i < linha.length(); i++) { // Percorre as strings lidas
 				if (linha[i] != ' ' && linha[i] != '.') { // Verifica se nao ha espaco ou ponto
+					palavra = palavra + linha[i];
 					palavra_ascii = palavra_ascii + ("%d", linha[i]); // Caso nao haja espaco ou ponto, vai somando os codigos ASCII em uma palavra
 				}
 				else {
 					
-					primeira_hash(palavra_ascii);
+					primeira_hash(palavra_ascii, palavra);
 
 				}
 			}
